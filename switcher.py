3e2ff2bccc17ec567811af2da1155f38c0907f93
yomeeliu <yomeeliu@tencent.com>
2015-06-09 17:36:08 +0800

    Provide config setting using sublime commands

---
+++ switcher.py
@@ -0,0 +1,24 @@
+import sublime, sublime_plugin
+from os import path
+
+SETTINGS_BASE_NAME = 'HiveOpener.sublime-settings'
+
+class HiveOpenSwitcherCommand(sublime_plugin.WindowCommand):
+    def run(self, **args):
+        if 'toggle_peek_file_on_highlight' in args:
+            self.toggle_peek_file()
+
+        if 'open_binary_file_in_sublime' in args:
+            binfile_open_in_subl = args.get('open_binary_file_in_sublime')
+            self.set_open_binary_file(binfile_open_in_subl)
+
+    def toggle_peek_file(self):
+        settings = sublime.load_settings(SETTINGS_BASE_NAME)
+        peek_file = not settings.get('peek_file_on_highlight', False)
+        settings.set('peek_file_on_highlight', peek_file)
+        sublime.save_settings(SETTINGS_BASE_NAME)
+
+    def set_open_binary_file(self, value):
+        settings = sublime.load_settings(SETTINGS_BASE_NAME)
+        settings.set('open_binary_file_in_sublime', value)
+        sublime.save_settings(SETTINGS_BASE_NAME)
