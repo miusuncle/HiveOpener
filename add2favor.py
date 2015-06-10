debda422bec3b1fc1d820a489807c7c355b736ab
miusuncle <miusuncle@gmail.com>
2015-06-10 23:35:12 +0800

    do some cleanup

--- add2favor.py
+++ add2favor.py
@@ -6,12 +6,13 @@ SETTINGS_BASE_NAME = 'HiveOpener.sublime-settings'
 class HiveAddToFavorCommand(sublime_plugin.WindowCommand):
     def run(self):
         settings = sublime.load_settings(SETTINGS_BASE_NAME)
-        index = self.index_in_list()
+        filename = self.get_file_name()
+        index = self.index_in_list(filename, settings)
 
         if index == -1:
             self.add_to_list(settings)
         else:
-            self.remove_from_list(settings, index)
+            self.remove_from_list(index, settings)
 
     def is_visible(self):
         return bool(self.get_file_name())
@@ -20,18 +21,17 @@ class HiveAddToFavorCommand(sublime_plugin.WindowCommand):
         filename = self.get_file_name()
         if not filename: return ''
 
-        if self.index_in_list() == -1:
+        if self.index_in_list(filename) == -1:
             return 'Add to Open List'
         else:
             return 'Remove from Open List'
 
-    def index_in_list(self, settings=None):
+    def index_in_list(self, filename, settings=None):
         if settings == None:
             settings = sublime.load_settings(SETTINGS_BASE_NAME)
 
         file_list = [item[0] for item in settings.get('files', [])]
-        filename = self.get_file_name()
-        return file_list.index(self.get_file_name()) if filename in file_list else -1
+        return file_list.index(filename) if filename in file_list else -1
 
     def add_to_list(self, settings):
         file_list = settings.get('files', [])
@@ -48,7 +48,7 @@ class HiveAddToFavorCommand(sublime_plugin.WindowCommand):
         settings.set('files', file_list)
         sublime.save_settings(SETTINGS_BASE_NAME)
 
-    def remove_from_list(self, settings, index):
+    def remove_from_list(self, index, settings):
         file_list = settings.get('files', [])
         file_list.pop(index)
         settings.set('files', file_list)
