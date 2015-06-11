773494a08e6357a9b3e2b0d074d540bca2007e99
yomeeliu <yomeeliu@tencent.com>
2015-06-11 18:04:25 +0800

    do NOT add `http://` prefix for url begin with `www`

--- add_context_url.py
+++ add_context_url.py
@@ -71,10 +71,7 @@ class AddContextUrlBaseCommand(sublime_plugin.TextCommand):
         for match in it:
             if match.start() <= (pt - line.a) and match.end() >= (pt - line.a):
                 url = text[match.start():match.end()]
-                if url[0:3] == 'www':
-                    return 'http://' + url
-                else:
-                    return url
+                return url
 
         return None
 
